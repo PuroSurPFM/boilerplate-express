6ed37ca43a0d14f986d405af0596503d9ff5cbb4
Puro Sur <purosurbxo@gmail.com>
2024-03-26 02:06:07 +0000

    served HTML File

--- myApp.js
+++ myApp.js
@@ -1,12 +1,10 @@
 let express = require('express');
 let app = express();
 console.log("Hello World");
-app.get("/", (req, res) => {
+app.get('/', function(req, res) {
   res.send("Hello Express");
+  res.sendFile(__dirname + '/views/index.html')
 });
-
-
-
 
 
 
