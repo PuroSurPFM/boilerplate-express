6c14a959623f1cc8b794d6a13958f0d14cf42592
Puro Sur <purosurbxo@gmail.com>
2024-03-26 03:49:42 +0000

    served static asset

--- myApp.js
+++ myApp.js
@@ -3,6 +3,7 @@ let app = express();
 console.log("Hello World");
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/views/index.html");
+app.use("/public", express.static(__dirname + "/public"))
 });
 
 
